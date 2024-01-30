e4acf343b7fa8b463210621779f8ecc2a478dc36
GianluGiarde <gianlugiarde@gmail.com>
2024-01-30 09:40:15 +0100

    Add users db

---
+++ config/library/users.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    [
+        'id' => '1',
+        'name' => 'Tony',
+        'last_name' => 'Stark',
+        'email' => 'ironman@gmail.com',
+        'password' => 'agengers',
+    ],
+    [
+        'id' => '2',
+        'name' => 'Gianluca',
+        'last_name' => 'Giardella',
+        'email' => 'gianlugiarde@gmail.com',
+        'password' => 'password',
+    ],
+];
