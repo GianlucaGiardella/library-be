9ce5423e32e75817c67ae76b40f42228a07a3cb0
GianluGiarde <gianlugiarde@gmail.com>
2024-01-30 09:40:01 +0100

    Add authors db

---
+++ config/library/authors.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'id' => '1',
+        'name' => 'Fëdor Dostoevskij',
+    ],
+    [
+        'id' => '2',
+        'name' => 'Gabriel García Márquez',
+    ],
+    [
+        'id' => '3',
+        'name' => 'Franz Kafka',
+    ],
+    [
+        'id' => '4',
+        'name' => 'Lev Tolstoj',
+    ],
+    [
+        'id' => '5',
+        'name' => 'William Shakespeare',
+    ],
+];
