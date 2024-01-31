3ed50aa0749a15b6b7dc3ca87f44faeeaa85271e
GianluGiarde <gianlugiarde@gmail.com>
2024-01-31 23:47:06 +0100

    Fix book config

---
+++ config/library/user_book.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    [
+        'id' => '1',
+        'user_id' => '1',
+        'book_id' => '1',
+        'readings' => '1',
+        'added_at' => '2024-01-30 09:00:00',
+        'deleted_at' => '2024-01-30 12:00:00',
+    ],
+    [
+        'id' => '2',
+        'user_id' => '1',
+        'book_id' => '3',
+        'readings' => '3',
+        'added_at' => '2024-01-30 09:00:00',
+        'deleted_at' => null,
+    ],
+    [
+        'id' => '3',
+        'user_id' => '1',
+        'book_id' => '4',
+        'readings' => '0',
+        'added_at' => '2024-01-30 09:00:00',
+        'deleted_at' => null,
+    ],
+    [
+        'id' => '4',
+        'user_id' => '1',
+        'book_id' => '7',
+        'readings' => '2',
+        'added_at' => '2024-01-30 09:00:00',
+        'deleted_at' => null,
+    ],
+    [
+        'id' => '5',
+        'user_id' => '2',
+        'book_id' => '2',
+        'readings' => '2',
+        'added_at' => '2024-01-30 09:00:00',
+        'deleted_at' => null,
+    ],
+    [
+        'id' => '6',
+        'user_id' => '2',
+        'book_id' => '4',
+        'readings' => '4',
+        'added_at' => '2024-01-30 09:00:00',
+        'deleted_at' => '2024-01-31 17:00:00',
+    ],
+    [
+        'id' => '7',
+        'user_id' => '2',
+        'book_id' => '5',
+        'readings' => '1',
+        'added_at' => '2024-01-30 09:00:00',
+        'deleted_at' => null,
+    ],
+];
