112e928bb7c7da9e13bd1578bf8cf1b9c5a25a65
GianluGiarde <gianlugiarde@gmail.com>
2024-02-06 03:23:29 +0100

    Fix user id and book paginate

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -4,44 +4,49 @@ namespace App\Http\Controllers\api;
 
 use App\Models\Book;
 use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Requests\BookRequest;
 use App\Http\Controllers\Controller;
 
 class BookController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $user = User::find(1);
-        $books = $user->books()->whereNull('deleted_at')->get();
+        $user = User::find($request->input('user_id'));
+        $books = $user->books()->whereNull('deleted_at')->paginate(8);
 
-        $booksFiltered = $books->map(function ($book) use ($user) {
+        // filtered user books
+        $books->getCollection()->transform(function ($book) use ($user) {
             return [
-                'title' => $book->title,
-                'author' => $book->author,
+                'id'        => $book->id,
+                'title'     => $book->title,
+                'author'    => $book->author,
                 'isbn_code' => $book->isbn_code,
-                'plot' => $book->plot,
-                'readings' => $user->books->find($book->id)->pivot->readings,
+                'plot'      => $book->plot,
+                'readings'  => $user->books->find($book->id)->pivot->readings,
             ];
         });
 
-        return response()->json($booksFiltered);
+        return response()->json($books);
     }
 
-    public function show(string $id)
+    public function show(Request $request, string $id)
     {
-        $user = User::find(1);
+        $user = User::find($request->input('user_id'));
         $book = Book::find($id);
 
+        // return 404 if not exists or is deleted
         if (!$book || !$user->books()->where('books.id', $book->id)->whereNull('deleted_at')->exists()) {
             return response()->json(['message' => 'Book not found'], 404);
         }
-
+        // filtered values
         $bookFiltered = [
-            'title' => $book->title,
-            'author' => $book->author,
+            'id'        => $book->id,
+            'title'     => $book->title,
+            'author'    => $book->author,
             'isbn_code' => $book->isbn_code,
-            'plot' => $book->plot,
-            'readings' => $user->books->find($book->id)->pivot->readings,
+            'plot'      => $book->plot,
+            'readings'  => $user->books->find($book->id)->pivot->readings,
         ];
 
         return response()->json($bookFiltered);
@@ -49,20 +54,22 @@ class BookController extends Controller
 
     public function store(BookRequest $request)
     {
-        $user = User::find(1);
+        $user = User::find($request->input('user_id'));
 
+        // create new book
         $book = new Book;
-        $book->title        = $request->title;
-        $book->author       = $request->author;
-        $book->isbn_code    = $request->isbn_code;
-        $book->plot         = $request->plot;
+        $book->title     = $request->title;
+        $book->author    = $request->author;
+        $book->isbn_code = $request->isbn_code;
+        $book->plot      = $request->plot;
         $book->save();
 
+        // create pivot 
         if ($book->save()) {
             $book->users()->attach($user->id, [
-                'readings'      => $request->readings,
-                'added_at'      => date('Y-m-d H:i:s'),
-                'deleted_at'    => null,
+                'readings'   => $request->readings,
+                'added_at'   => date('Y-m-d H:i:s'),
+                'deleted_at' => null,
             ]);
         }
 
@@ -71,19 +78,22 @@ class BookController extends Controller
 
     public function update(BookRequest $request, string $id)
     {
-        $user = User::find(1);
+        $user = User::find($request->input('user_id'));
         $book = Book::find($id);
 
+        // return 404 if not exists or is deleted
         if (!$book || !$user->books()->where('books.id', $book->id)->whereNull('deleted_at')->exists()) {
             return response()->json(['message' => 'Book not found'], 404);
         }
 
-        $book->title        = is_null($request->title) ? $book->title : $request->title;
-        $book->author       = is_null($request->author) ? $book->author : $request->author;
-        $book->isbn_code    = is_null($request->isbn_code) ? $book->isbn_code : $request->isbn_code;
-        $book->plot         = is_null($request->plot) ? $book->plot : $request->plot;
+        // update book
+        $book->title      = is_null($request->title) ? $book->title : $request->title;
+        $book->author     = is_null($request->author) ? $book->author : $request->author;
+        $book->isbn_code  = is_null($request->isbn_code) ? $book->isbn_code : $request->isbn_code;
+        $book->plot       = is_null($request->plot) ? $book->plot : $request->plot;
         $book->save();
 
+        // update pivot
         if ($book->save()) {
             $book->users()->updateExistingPivot($user->id, ['readings' => $request->readings]);
         }
@@ -91,17 +101,19 @@ class BookController extends Controller
         return response()->json(['message' => 'Book updated'], 200);
     }
 
-    public function destroy(string $id)
+    public function destroy(Request $request, string $id)
     {
-        $user = User::find(1);
+        $user = User::find($request->input('user_id'));
         $book = Book::find($id);
 
+        //check if exists or is already deleted
         if (!$book) {
             return response()->json(['message' => 'Book not found'], 404);
         } else if ($user->books()->where('books.id', $book->id)->whereNotNull('deleted_at')->exists()) {
             return response()->json(['message' => 'Book already deleted'], 404);
         }
 
+        // update deleted_at
         $book->users()->updateExistingPivot($user->id, ['deleted_at' => date('Y-m-d H:i:s')]);
 
         return response()->json(['message' => 'Book deleted'], 200);
@@ -109,9 +121,10 @@ class BookController extends Controller
 
     public function add(BookRequest $request, string $id)
     {
-        $user = User::find(1);
+        $user = User::find($request->input('user_id'));
         $book = Book::find($id);
 
+        //check if exists or is already added
         if (!$book) {
             return response()->json(['message' => 'Book not found'], 404);
         } else if ($user->books()->where('books.id', $book->id)->whereNull('deleted_at')->exists()) {
@@ -121,16 +134,16 @@ class BookController extends Controller
         if ($user->books()->where('books.id', $book->id)->whereNotNull('deleted_at')->exists()) {
             // update
             $book->users()->updateExistingPivot($user->id, [
-                'readings'      => $request->readings,
-                'added_at'      => date('Y-m-d H:i:s'),
-                'deleted_at'      => null,
+                'readings'   => $request->readings,
+                'added_at'   => date('Y-m-d H:i:s'),
+                'deleted_at' => null,
             ]);
         } else {
             // store
             $book->users()->attach($user->id, [
-                'readings'      => $request->readings,
-                'added_at'      => date('Y-m-d H:i:s'),
-                'deleted_at'    => null,
+                'readings'   => $request->readings,
+                'added_at'   => date('Y-m-d H:i:s'),
+                'deleted_at' => null,
             ]);
         }
 
